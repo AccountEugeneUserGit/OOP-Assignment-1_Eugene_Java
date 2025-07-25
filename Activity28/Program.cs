1223cf0eda0ff46c121a4cd22c4c28d82499b9be
MicrowaveTuesday <user.eugene1acc@gmail.com>
2025-07-26 05:35:11 +0800

    Add files via upload

---
+++ Activity28/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int limit = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 1; i < limit; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine($"Sum of multiples of 3 or 5 below {limit}: {sum}");
+    }
+}
