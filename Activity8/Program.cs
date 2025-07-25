c070ea85cdb8d4e1ab86fc21cb6827622f7ca63f
MicrowaveTuesday <user.eugene1acc@gmail.com>
2025-07-26 05:27:41 +0800

    Add files via upload

---
+++ Activity8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a numeric string: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            Console.WriteLine($"Result after adding 10: {number + 10}");
+        }
+        else
+        {
+            Console.WriteLine("Error: Input is not a valid number.");
+        }
+    }
+}
+
