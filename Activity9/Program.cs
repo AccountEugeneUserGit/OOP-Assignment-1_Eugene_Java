fde9645e89c7cb175ae461830955b7f9dee74984
MicrowaveTuesday <user.eugene1acc@gmail.com>
2025-07-26 05:29:14 +0800

    Add files via upload

---
+++ Activity9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            if (number % 2 == 0)
+                Console.WriteLine("The number is even.");
+            else
+                Console.WriteLine("The number is odd.");
+        }
+        else
+        {
+            Console.WriteLine("Error: Input is not an integer.");
+        }
+    }
+}
+
